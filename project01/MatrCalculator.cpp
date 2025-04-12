f7a59950831e21ce70979f6ff6a60c45bda8948f
DiCeka <diceka2500@gmail.com>
2025-04-12 17:36:04 +0300

    Добавил "функционал" к матричному калькулятору

--- project01/MatrCalculator.cpp
+++ project01/MatrCalculator.cpp
@@ -1,5 +1,12 @@
+#pragma once
 #include "MatrCalculator.h"
+#include <iostream>
+#include "iotools.h"
+using namespace std;
 
 void MatrCalculator()
 {
+	cout << "\nДобро пожаловать в матричный калькулятор";
+	sleep(3);
+
 }
