de4a72ff93a3b9628cb22e4b80e7de0bb56957f5
FANSREAPER <modeyegor0@gmail.com>
2025-04-13 01:28:54 +0300

    iotools обновлён функциями: iotools_Int_InputInRange - регистрация ввода в или вне диапазона iotools_Int_InputMoreLessThanValue - регистрация ввода больше/меньше числа CinQualification - обновлено название у функции обработки чистки потокового вввода в случае несоответствия типа данных у записываемой переменной и введённого значения

--- project01/project01.cpp
+++ project01/project01.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 int main()
 {
+    cout << "TESTTAST\n";
     setlocale(LC_ALL, "ru");
     // переменная, предназначенная для выбора пользователем нужного калькулятора
     int selecter = iotools_Int_InputInRange("Какой калькулятор хотите использовать?\n\t 1 - Матричный калькулятор\n\t 2 - Калькулятор формул комбинаторики\n",
-        "ERROR: Такого калькулятора нет!", 1, 2, 12);
-
+        "ERROR: Такого калькулятора нет!", 1, 2, 6);
+    
     switch (selecter)
     {
     case 1: MatrCalculator(); break;
-    // case 2: CombinationCalculator(); break;
+        // case 2: CombinationCalculator(); break;
     }
-    
-    return 0; 
+
+    return 0;
 }
